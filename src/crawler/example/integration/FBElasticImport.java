904cd743894e8f2b7e07f8b6d5edbdaef558839f
scropiosky <scopiosky@gmail.com>
2017-04-16 21:58:35 +0800

    output

--- src/crawler/example/integration/FBElasticImport.java
+++ src/crawler/example/integration/FBElasticImport.java
@@ -4,6 +4,10 @@ import com.github.abola.crawler.CrawlerPack;
 import com.mashape.unirest.http.Unirest;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
+import java.io.BufferedOutputStream;
+import java.io.FileOutputStream;
+import java.io.PrintStream;
+
 
 /**
  * Created by Abola Lee on 2016/7/10.
@@ -12,21 +16,26 @@ public class FBElasticImport {
 
     static String elasticHost = "localhost" ;
     static String elasticPort = "9200" ;
-    static String elasticIndex = "fgo-1";
+    static String elasticIndex = "tb_fgo_1";
     static String elasticIndexType = "data";
     static String pageName = "FateGO.TW";
-    static long start = 1492096441;
-    static int days = 30;
+    static long start = 1492345280;
+    static int days = 60;
 
     public static void main(String[] args) {
+
         for (long datatime = start ; datatime > start-86400*days ;datatime-=3600*8) {
             String uri =
                     "https://graph.facebook.com/v2.8"
-                            + "/"+pageName +"/feed?fields=comments,from&since="+(datatime-3600*8)+"&until="+datatime+"&limit=100"
+                            + "/"+pageName +"/feed?fields=comments,from,created_time&since="+(datatime-3600*8)+"&until="+datatime
                             + "&access_token=1289426837805412%7Cae451649927ef2a59a839574cba8e175";
 
 
             try {
+                PrintStream out = new PrintStream(new
+                        BufferedOutputStream(new
+                        FileOutputStream("c:\\fgo_1.txt", true)));
+
 
                 Elements elems =
                         CrawlerPack.start()
@@ -34,13 +43,14 @@ public class FBElasticImport {
                                 .select("data:has(created_time)");
 //              System.out.println(elems);
 //              String output = "id,名稱,按讚數,討論人數\n";
-                System.out.println(elems.size());
-                // 遂筆處理
+//                System.out.println(elems.size());
+//                // 遂筆處理
                 for (Element data : elems) {
 
-
+//                    SimpleDateFormat sdFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
                     String created_time = data.select("created_time").text();
                     String id = data.select("from > id").text();
+//                    String idtime = data.select("from > id,data data created_time").text();
 //                    String message = data.select("message").text();
 //                    String likes = data.select("likes > summary > total_count").text();
 //                    String comments = data.select("comments > summary > total_count").text();
@@ -56,23 +66,32 @@ public class FBElasticImport {
                     // Elasticsearch data format
 
 
-                    String elasticJson = "{" +
-                            "\"created_time\":\"" + created_time + "\"" +
-//                            ",\"message\":\"" + message + "\"" +
-//                            ",\"likes\":" + likes +
-                            ",\"id\":\"" + id + "\"" +
-                            ",\"pagename\":\"" + pageName + "\"" +
-//                            ",\"comments\":" + comments +
-                            "}";
+//                    String elasticJson = "{" +
+//                            "\"created_time\":\"" + created_time + "\"" +
+//////                            ",\"message\":\"" + message + "\"" +
+//////                            ",\"likes\":" + likes +
+//                            ",\"id\":\"" + id + "\"" +
+//                            ",\"pagename\":\"" + pageName + "\"" +
+//////                            ",\"comments\":" + comments +
+//                            "}";
 
 
-                    System.out.println(
-                            // curl -XPOST http://localhost:9200/pm25/data -d '{...}'
-                            sendPost("http://" + elasticHost + ":" + elasticPort
-                                            + "/" + elasticIndex + "/" + elasticIndexType
-                                    , elasticJson));
-//                    System.out.println(elasticJson);
+                    String elasticJson =  created_time + "," + id +"," + pageName +"\n";
+
+//                    System.out.println(
+//                            // curl -XPOST http://localhost:9200/pm25/data -d '{...}'
+//                            sendPost("http://" + elasticHost + ":" + elasticPort
+//                                            + "/" + elasticIndex + "/" + elasticIndexType
+//                                    , elasticJson));
+//                System.out.println(elasticJson);
+//                    System.setOut(out); // 轉換輸出流
+//                    System.out.println(elasticJson); // 將輸出傳進文件
+                    out.println(elasticJson);
+
                 }
+
+                out.close();
+
             }catch(Exception e){}
 
         }
@@ -99,3 +118,5 @@ public class FBElasticImport {
         }catch(Exception e){return "Error:" + e.getMessage();}
     }
 }
+
+
